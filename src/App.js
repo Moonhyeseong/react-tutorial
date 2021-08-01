c0b90337fb648fdcf776117c4b4eb23cefa8c957
Moonhyeseong <moonhs97@gmail.com>
2021-08-01 22:08:14 +0900

    1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello, React!!</h1>
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
   }
