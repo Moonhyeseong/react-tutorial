ad1e09f2f60a14851c06d98616df16a07866b541
Moonhyeseong <moonhs97@gmail.com>
2021-08-01 16:52:02 +0900

    1.0
    1.0

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
